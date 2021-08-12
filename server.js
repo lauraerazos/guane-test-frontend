46d7e7d7436ed759dab153624fd51718a046e139
Laura Erazo <laura.erazo@udea.edu.co>
2021-08-11 22:31:04 -0500

    Cambios en server

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/guane-test-frontend'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/guane-test-frontend/index.html'));
+app.use(express.static(__dirname +'/dist/guane-test-frontend'));
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname +'/dist/guane-test-frontend/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('/dist/guane-test-frontend'));
+}
+var port = process.env.PORT || 8080;
+
+app.listen(port);
+console.log('server started '+ port);
